16390666e1c37a4bc83c3363561730fb6317b559
Shiv25z <shivamzagade25899@gmail.com>
2022-09-19 20:37:47 +0530

    Added: Welcome to Snake & Ladder

---
+++ SnakeLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class SnakeLadder {
+
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Snake and Ladder Game! ");
+
+
+                }
+
+        }
