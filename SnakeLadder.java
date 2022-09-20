787098040d4808c8ac433e93573c97231a7c0acb
Shiv25z <shivamzagade25899@gmail.com>
2022-09-20 14:00:05 +0530

    Added: UC4

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -11,33 +11,27 @@ public class SnakeLadder {
 
 
         int player = 0;// uc1
-        System.out.println("Player position => "+player);
+        System.out.println("Player position => " + player);
 
-        int dice = random.nextInt(6)+1;// uc2
+        //int dice = random.nextInt(6) + 1;// uc2
 
 
-        int game = random.nextInt(3)+1; // uc3
+        while (player != 100) {
+            int dice = random.nextInt(6) + 1;// uc2
+            int game = random.nextInt(3) + 1; // uc3
+            switch (game) {
+                case 1:// uc4
+                    player = player + 0;  // No Play Case
+                    System.out.print(" Dice shows: 0");
+                    System.out.println(" New Score of Player is => " + player);
+                    System.out.println("");
+                    break;
 
-        switch(game){
-            case 1:
-                player +=0;
-                System.out.println("New Score of Player is => "+player);
-                break;
 
-            case 2:
-                player = player + dice;
-                System.out.println("New Score of Player is => "+player);
-                break;
+            }
 
-            case 3:
-                player = player - 2 * dice;
-                System.out.println("New Score of Player is => "+player);
-                break;
         }
-
-
-
-
     }
-
 }
+
+
